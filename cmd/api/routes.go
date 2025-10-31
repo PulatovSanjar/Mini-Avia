236a59131f9bcb2859368b169132cca550f92276
Sanjar.Pulatov <sanjar.pulatov@alif.uz>
2025-10-31 17:02:06 +0500

    created middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,71 +1,60 @@
-package main
+package middleware
 
-import "net/http"
-
-func routes() http.Handler {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong"))
-	})
-	return mux
-}
+import (
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log/slog"
+	"net/http"
+)
 
 //
-//import (
+//func routes() http.Handler {
+//	mux := http.NewServeMux()
+//	mux.HandleFunc("GET /ping", func(w http.ResponseWriter, r *http.Request) {
+//
+//		w.Write([]byte("pong"))
+//	})
+//	return mux
+//}
+
+// import (
+//
 //	"Mini-Avia/internal/bookings"
 //	"context"
 //	"log/slog"
 //	"net/http"
 //
 //	"github.com/jackc/pgx/v5/pgxpool"
-//)
 //
-//// only ограничивает метод (аналог Allow в фреймворках)
+// )
 //
+// // only ограничивает метод (аналог Allow в фреймворках)
 //
-////// пример "param" роутинга без внешних либ (для /bookings/{id})
-////func withBookingID(base string, h http.Handler) http.Handler {
-////	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-////		// /api/v1/bookings/<id>
-////		if !strings.HasPrefix(r.URL.Path, base+"/bookings/") {
-////			http.NotFound(w, r)
-////			return
-////		}
-////		id := strings.TrimPrefix(r.URL.Path, base+"/bookings/")
-////		if id == "" || strings.Contains(id, "/") {
-////			http.NotFound(w, r)
-////			return
-////		}
-////		ctx := context.WithValue(r.Context(), bookings.CtxKeyID{}, id)
-////		h.ServeHTTP(w, r.WithContext(ctx))
-////	})
-////}
-//
-//func routes(pool *pgxpool.Pool, log *slog.Logger) http.Handler {
-//	handler := &bookings.Handler{}
-//	router := http.NewServeMux()
-//	//base := "/api/v1"
-//
-//	router.HandleFunc("POST /bookings", handler.findById)
-//
-//	//// GET /api/v1/offers?from=...&to=...&date=YYYY-MM-DD
-//	router.HandleFunc(base+"/offers", api.only(http.MethodGet,
-//	//	offers.ListHandler(pool, log)))
-//	//
-//	//// POST /api/v1/bookings
-//	//mux.Handle(base+"/bookings", only(http.MethodPost,
-//	//	bookings.CreateHandler(pool, log)))
-//	//
-//	//// GET /api/v1/bookings/{id} (пример параметра path)
-//	//mux.Handle(base+"/bookings/", only(http.MethodGet,
-//	//	withBookingID(base, bookings.GetByIDHandler(pool, log))))
-//	//
-//	//// POST /api/v1/tickets/issue
-//	//mux.Handle(base+"/tickets/issue", only(http.MethodPost,
-//	//	tickets.IssueHandler(pool, log)))
-//
-//	// можно добавить /ping без БД, если нужно
-//	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("pong")) })
-//
-//	return mux
-//}
+// //// пример "param" роутинга без внешних либ (для /bookings/{id})
+// //func withBookingID(base string, h http.Handler) http.Handler {
+// //	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// //		// /api/v1/bookings/<id>
+// //		if !strings.HasPrefix(r.URL.Path, base+"/bookings/") {
+// //			http.NotFound(w, r)
+// //			return
+// //		}
+// //		id := strings.TrimPrefix(r.URL.Path, base+"/bookings/")
+// //		if id == "" || strings.Contains(id, "/") {
+// //			http.NotFound(w, r)
+// //			return
+// //		}
+// //		ctx := context.WithValue(r.Context(), bookings.CtxKeyID{}, id)
+// //		h.ServeHTTP(w, r.WithContext(ctx))
+// //	})
+// //}
+func loadRoutes(pool *pgxpool.Pool) http.Handler {
+	handler := &bookings.Handler{}
+	router := http.NewServeMux()
+	//base := "/api/v1"
+
+	router.HandleFunc("POST /bookings", handler.findById)
+
+	// можно добавить /ping без БД, если нужно
+	router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("pong")) })
+
+	return router
+}
